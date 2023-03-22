499e0b49eab16c96e6bb4862287009d3d060bb1c
Baraga,James Cedrick <53712813+jamesced@users.noreply.github.com>
2023-03-22 16:43:58 +0800

    Create building_facilities_request.php
    updated laravel migration

---
+++ migrations/building_facilities_request.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('building_facilities_request', function (Blueprint $table) {
+            $table->id();
+            $table->string('requesting_party');  
+            $table->date('date_requested');
+            $table->time('time_requested');
+            $table->string('requested_facility');
+            $table->date('date_from');
+            $table->date('date_to');
+            $table->time('activity_time');
+            $table->integer('pdea_participants');
+            $table->string('services');
+            $table->string('matertial_needed');
+            $table->integer('other_participants');
+            $table->string('non_pdea_material');
+            $table->string('status');
+            $table->softDeletes();
+            $table->timestamps();
+
+            //done
+            // rename fields
+            //     time_of_activity - activity_time
+            //     number_of_participant_pdea - pdea_participants
+            //     number_of_participant_other - other_participants
+            //     service_office - services
+            //     equipment_matertial_needed - matertial_needed
+            //     non_pdea_equipment - non_pdea_material
+            // add soft deletes ($table->softDeletes())
+            // add status field
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
